39e9fcf18fdac1af7c2e5e700f83be55c3b29cdb
Mikhail Filippov <mikhail@filippov.me>
2012-05-06 10:42:35 +0400

    Update netty.

--- src/main/java/me/filippov/jretracker/App.java
+++ src/main/java/me/filippov/jretracker/App.java
@@ -1,13 +1,13 @@
 package me.filippov.jretracker;
 
 /**
- * Hello world!
+ * Main application class.
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
     }
 }
