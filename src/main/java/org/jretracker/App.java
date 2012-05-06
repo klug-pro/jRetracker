70cada10682283f0f1ad4bfe02713262d6e69c94
Mikhail Filippov <mikhail@filippov.me>
2012-05-06 15:20:25 +0400

    Add parameters loading.

--- src/main/java/org/jretracker/App.java
+++ src/main/java/org/jretracker/App.java
@@ -2,12 +2,27 @@ package org.jretracker;
 
 /**
  * Main application class.
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
 
+    private int port;
+    private final static CliHandler cliHandler = new CliHandler();
+
+    public App(int port) {
+        this.port = port;
+    }
+
+    public void run() {
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Load config.
+        cliHandler.parse(args);
+        // Show help.
+        if (cliHandler.isHelp()) {
+            System.out.println("Usage: App [--port=Port number]");
+        }
+        new App(cliHandler.getPort()).run();
     }
 }
